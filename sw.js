a269dd2e3a0811ca981ee2b1998d98a02456199a
Dormaan-oss <derhouzztmeistda.mp@gmail.com>
2026-05-26 21:45:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,25 @@
-const CACHE = 'mka-kurzaudit-v4';
-const FILES = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/favicon.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return Promise.all(
-        FILES.map(url =>
-          fetch(url).then(r => cache.put(url, r)).catch(() => {})
-        )
-      );
-    })
-  );
-  self.skipWaiting();
+const CACHE = 'mka-kurzaudit-v3-v1';
+const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./icons/favicon.png'];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>
+    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+  ).then(()=>self.clients.claim()));
 });
-
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', e=>{
+  if(e.request.method!=='GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(response => {
-        if (!response || response.status !== 200) return response;
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, copy));
-        return response;
-      }).catch(() => caches.match('./index.html'));
+    caches.match(e.request).then(cached=>{
+      const net=fetch(e.request).then(res=>{
+        if(res&&res.status===200&&res.type==='basic'){
+          const clone=res.clone();
+          caches.open(CACHE).then(c=>c.put(e.request,clone));
+        }
+        return res;
+      }).catch(()=>cached);
+      return cached||net;
     })
   );
 });
